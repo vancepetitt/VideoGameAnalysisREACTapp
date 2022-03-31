d4c690f49975c83e82c24521ffe0b2ae56f4f403
Kat <katdonley1@gmail.com>
2022-03-31 10:23:28 -0600

    Kat: installed axios and created chart componenets.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, {useState, useEffect} from "react";
+import axios from 'axios';
 
 
 
@@ -7,7 +8,12 @@ function App() {
 
   const[videoGames, setVideoGames] = useState('');
 
-  
+  async function getAllGames(prop){
+    let response = await axios.get("https://localhost:7260/api/games");
+    // 57067
+    setGames(response.data);
+    console.log(response.data);
+  };
 
   return (
     <div className="App">
