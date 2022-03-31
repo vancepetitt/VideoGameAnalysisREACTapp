8e533513175fa05989d6ccf6845df62cdbb0a723
vance.petitt <vance.petitt@gmail.com>
2022-03-31 10:19:21 -0500

    Created component folders for search bar, nav bar, and a display for a single video game information.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react";
+
+
 
 function App() {
+
+  const[videoGames, setVideoGames] = useState('');
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Video Game Analysis</h1>
+
     </div>
   );
 }
